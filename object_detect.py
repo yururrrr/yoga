e890f151485647bea0a5ca5a03cdc0ee9eddf416
yururrrr <bella12.2977@gmail.com>
2024-04-25 12:24:10 +0800

    draw bounding box

--- object_detect.py
+++ object_detect.py
@@ -5,7 +5,10 @@ from torchvision.transforms import functional as F
 import argparse
 import os
 import pandas as pd
-import numpy as np
+import time
+# from utils._tool import crop
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -13,7 +16,6 @@ def parse_args():
     parser.add_argument('--save_path', type=str, help='save folder path')
 
     return parser.parse_args()
-    # os.makedirs(args.save_path, exist_ok=True)
 
 # Preprocess input image
 def preprocess_image(image):
@@ -24,7 +26,7 @@ def preprocess_image(image):
 # Perform inference
 def detect_objects(model, frame):
     # Convert the frame to a PyTorch tensor
-    image_tensor = torch.from_numpy(frame).permute(2, 0, 1).float()  # Convert to CHW format
+    image_tensor = torch.from_numpy(frame).permute(2, 0, 1).float().to(device)  # Convert to CHW format
     image_tensor /= 255.0  # Normalize to [0, 1]
     image_tensor = image_tensor.unsqueeze(0)  # Add batch dimension
 
@@ -36,64 +38,81 @@ def detect_objects(model, frame):
 
 # Visualize results
 def visualize_result(frame, predictions):
-    max_score = 0
+    max_area = 0
+    min_x = 2000
+    selected_box = 0
+    max_width = 640
     
     for box, label, score in zip(predictions[0]['boxes'], predictions[0]['labels'], predictions[0]['scores']):
         xmin, ymin, xmax, ymax = box.tolist()  # Convert tensor to list
         xmin, ymin, xmax, ymax = int(xmin), int(ymin), int(xmax), int(ymax)  # Convert to integers
+        cen_x = xmax-xmin
         if label.item() == 1 and score.item() > 0.5:  # Filter for 'person' class
-            if score.item() > max_score:
-                max_score = score.item()
+            area = (xmax - xmin)*(ymax - ymin)
+            if area > max_area and xmin < min_x:
+                max_area = area
+                min_x = xmin
+                selected_box = (xmin, ymin, xmax, ymax)
 
-                # Calculate the larger square bounding box
-                width = xmax - xmin
-                height = ymax - ymin
-                max_dim = max(width, height)
-                center_x = xmin + width // 2
-                center_y = ymin + height // 2
-                square_x1 = center_x - max_dim // 2
-                square_y1 = center_y - max_dim // 2
-                square_x2 = square_x1 + max_dim
-                square_y2 = square_y1 + max_dim
-                frame = cv2.rectangle(frame, (square_x1, square_y1), (square_x2, square_y2), (0, 255, 0), 2)
-                
-                # Crop the frame to the square bounding box
-                cropped_frame = frame[square_y1:square_y2, square_x1:square_x2]
-            else:
-                cropped_frame = None
+    square_x1, square_y1, square_x2, square_y2 = 0, 0, 0, 0
+    if selected_box:
+        xmin, ymin, xmax, ymax = selected_box            
+        # Calculate the larger square bounding box
+        width = xmax - xmin
+        height = ymax - ymin
+        max_dim = max(width, height)
+        center_x = xmin + width // 2
+        center_y = ymin + height // 2
+        square_x1 = center_x - max_dim // 2 - max_dim // 4
+        square_y1 = center_y - max_dim // 2 - max_dim // 4
+        square_x2 = square_x1 + max_dim + max_dim // 2
+        square_y2 = square_y1 + max_dim + max_dim // 2
 
-    return frame, cropped_frame
+        frame = cv2.rectangle(frame, (square_x1, square_y1), (square_x2, square_y2), (0, 255, 0), 2)
+        
+        return frame, [square_x1, square_y1, square_x2, square_y2]
+    else : return frame, None
 
-def video2class(pose, output_path, v_num):
+def video2class(pose, v_num, save_path):
     
-    if pose ==1:    output_frame_path = f'{output_path}mountain pose/{v_num}/'
-    elif pose ==2:  output_frame_path = f'{output_path}raised hands pose/{v_num}/'
-    elif pose ==3:  output_frame_path = f'{output_path}standing forward bend/{v_num}/'
-    elif pose==4:   output_frame_path = f'{output_path}half standing forward bend/{v_num}/'
-    elif pose==5:   output_frame_path = f'{output_path}plank/{v_num}/'
-    elif pose==6:   output_frame_path = f'{output_path}four-limbed staff pose/{v_num}/'
-    elif pose==7:   output_frame_path = f'{output_path}upward facing dog/{v_num}/'
-    elif pose==8:   output_frame_path = f'{output_path}downward facing dog/{v_num}/'
+    if pose ==1:    output_frame_path = f'mountain pose/{v_num}/'
+    elif pose ==2:  output_frame_path = f'raised hands pose/{v_num}/'
+    elif pose ==3:  output_frame_path = f'standing forward bend/{v_num}/'
+    elif pose==4:   output_frame_path = f'half standing forward bend/{v_num}/'
+    elif pose==5:   output_frame_path = f'plank/{v_num}/'
+    elif pose==6:   output_frame_path = f'four-limbed staff pose/{v_num}/'
+    elif pose==7:   output_frame_path = f'upward facing dog/{v_num}/'
+    elif pose==8:   output_frame_path = f'downward facing dog/{v_num}/'
     
-    os.makedirs(output_frame_path, exist_ok=True)
-    return output_frame_path
+    out = os.path.join(save_path, output_frame_path)
+    os.makedirs(out, exist_ok=True)
+    return out
 
 def main():
     args = parse_args()
-    if args.data_path is None or not os.path.exists(args.data_path):
-        print("Please provide a valid data path.")
-        return
     
-    home_path = 'C:/Users/VIPLAB/Desktop/yuru/'
+    
+    home_path = 'C:/Users/VIP/Desktop/yuru/yoga/'
     data_path = os.path.join(home_path, args.data_path)
+    save_path = os.path.join(home_path, args.save_path)
+    
+    if args.data_path is None or not os.path.exists(data_path):
+        print("Please provide a valid data path.")
+        print(data_path)
+        return
+    os.makedirs((save_path), exist_ok=True)
+
     video_files = [f for f in os.listdir(data_path) if f.endswith('.MP4')]
 
     labels=['downward facing dog', 'four-limbed staff pose', 'half standing forward bend', 'mountain pose', 'plank', 'raised hands pose', 'standing forward bend', 'upward facing dog']
-    for la in labels: os.makedirs(args.save_path+la, exist_ok=True)
+    for la in labels: os.makedirs(os.path.join(home_path, args.save_path, la), exist_ok=True)
     
     df = pd.read_excel(home_path+'yoga-pose-classification/movement_frames_info.xlsx', sheet_name='train')
     
     v_num = 0
+    start_time = time.time()
+    model = fasterrcnn_resnet50_fpn(pretrained=True).to(device)
+    model.eval()  # Set the model to evaluation mode
     for video_file in video_files:
         cap = cv2.VideoCapture(os.path.join(data_path, video_file))
         if not cap.isOpened():
@@ -107,47 +126,60 @@ def main():
         fps = cap.get(cv2.CAP_PROP_FPS)
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        out = cv2.VideoWriter(os.path.join(args.save_path, video_file), cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
-        
+        out = cv2.VideoWriter(os.path.join(save_path, video_file), cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+   
         # Process each frame of the video
-        count = 0
-        index = 0
+        frame_count = 0
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
-
-            # Load the pre-trained model
-            model = fasterrcnn_resnet50_fpn(pretrained=True)
-            model.eval()  # Set the model to evaluation mode
-            
+            # if frame_count % 15 != 0:
+            #     continue
             # Perform object detection on the frame
-            predictions = detect_objects(model, frame)
-            frame_with_boxes, cropped_frame = visualize_result(frame, predictions)
-            out.write(frame_with_boxes)
+            if frame_count % 15 == 0:
+                predictions = detect_objects(model, frame)
+            frame_with_boxes, box = visualize_result(frame, predictions)
             
             # svae the cropped frame
-            if cropped_frame!=None and count >= rows.iloc[index, 2] and count <= rows.iloc[index, 3]:
-                pose = rows.iloc[index, 1]
-                class_path = video2class(pose, args.save_path, v_num)
-                if count==rows.iloc[index, 3] : 
-                    index+=1
-                    v_num+=1
-                    print(f'fisnied video {video_name} pose={pose}')
-                if index==12: 
-                    print(f'----------finished video {video_name}----------')
-                    break
-                cv2.imwrite(f'{class_path}{video_name}_{count}.png', cropped_frame)
+            # if box is not None:  # Check if frame_with_boxes is not None
+            #     if count >= rows.iloc[index, 2] and count <= rows.iloc[index, 3]:
+            #         pose = rows.iloc[index, 1]
+            #         class_path = video2class(pose, v_num, save_path)
+            #         if count == rows.iloc[index, 3]:
+            #             index += 1
+            #             v_num += 1
+            #             print(f'Finished video {video_name}, pose={pose}')
+            #         if index == 12: 
+            #             break
+
+            #         cropped_frame = frame[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+            #         # print(box)
+            #         # print(cropped_frame)
+            #         if cropped_frame is not None:  # Check if cropped_frame is not None
+            #             # print('here')
+            #             if cropped_frame.shape[0] > 0 and cropped_frame.shape[1] > 0:
+            #                 path = os.path.join(class_path, f'{video_name}_{count}.png')
+            #                 # print(path)
+            #                 cv2.imwrite(path, cropped_frame)
+            
+            
+            out.write(frame_with_boxes)
+            cv2.imshow('Object Detection', frame_with_boxes)
 
             # Display the frame (optional)
-            cv2.imshow('Object Detection', frame_with_boxes)
+            
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            count+=1
+            frame_count+=1
 
         # Release video capture and writer objects
         cap.release()
         out.release()
+        end_time = time.time()
+        print(f'----------finished video {video_name}----------{end_time-start_time:.2f} seconds')
+        print()
+        frame_count+=1
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
--- pose.py
+++ pose.py
@@ -1,6 +1,4 @@
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
 import cv2
 import numpy as np
 import math
@@ -8,10 +6,17 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+import argparse
+from scipy.signal import savgol_filter
 
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
-from dataset import VideoDataset
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, help='video folder path')
+    parser.add_argument('--save_path', type=str, help='save folder path')
+
+    return parser.parse_args()
+
 
 def cal_angle(point_a , point_b, point_c):
     x1, y1, z1 = (point_a.x - point_b.x), (point_a.y - point_b.y), (point_a.z - point_b.z)
@@ -27,87 +32,199 @@ def cal_angle(point_a , point_b, point_c):
     return B
 
 model_path = 'pose_landmark.task'
+       
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=True)
 
-mp_drawing = mp.solutions.drawing_utils        
-mp_drawing_styles = mp.solutions.drawing_styles  
-mp_pose = mp.solutions.pose                     
-
-points = ['L_ankle', 'R_ankle', 'L_knee', 'R_knee', 'L_hip', 'R_hip', 'L_shoulder', 'R_shoulder', 'L_elbow', 'R_elbow', 'L_wrist', 'R_wrist']
-
-
-home_path = 'C:/Users/VIPLAB/Desktop/yuru/'
-data_path = home_path+'yoga-pose-classification/dataset_mat'
-
-def detect_keypoints(img_path):
-    mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose(static_image_mode=True)
+def detect_keypoints(img):
     
-    img = cv2.imread(img_path)
+    # img = cv2.imread(img_path)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
     results = pose.process(img_rgb)
-    
-    # keypoints = []
-    # if results.pose_landmarks:
-    #     for landmark in results.pose_landmarks.landmark:
-    #         keypoints.append((landmark.x, landmark.y, landmark.z))
 
     return results.pose_landmarks
 
+def plot_points(data, point, path, rows):
+    os.makedirs(path, exist_ok=True)
+    plt.figure(figsize=(30,15), dpi=100, linewidth = 8)
+    y_smooth = savgol_filter(data, 30, 2)
+    
+    plt.plot(data, alpha=0.2, color='b')
+    plt.plot(y_smooth, color='b')
+    plt.xlabel('time')
+    plt.ylabel('Position')
+    plt.title(point, fontsize=30)
 
-labels = sorted(os.listdir(data_path))
-for label in labels:
-    label_path = os.path.join(data_path, label)
-    videos = os.listdir(label_path)
-    for video in videos:
-        video_path = os.path.join(label_path, video)
-        frames = os.listdir(video_path)
-        joints = {r_ankle:[], l_ankle:[], r_knee:[], l_knee:[], r_hip:[], l_hip:[],
-                  r_shoulder:[], l_shoulder:[], r_elbow:[], l_elbow:[], r_wrist:[], l_wrist:[]}
-        r_ankle = []
-        l_ankle = []
-        r_knee = []
-        l_knee = []
-        r_hip = []
-        l_hip = []
-        r_shoulder = []
-        l_shoulder = []
-        r_elbow = []
-        l_elbow = []
-        r_wrist = []
-        l_wrist = []
-        
-        for frame in frames:
-            frame_path = os.path.join(video_path, frame)
-            keypoint = detect_keypoints(frame_path)
+    # print(' not in loop')
+    for i in range(len(rows)):
+        # print('in loop')
+        pose = rows.loc[i, 'Pose']
+        start = rows.loc[i, 'start']
+        end = rows.loc[i, 'end']
+        # print('in row')
+        plt.axvline(x=start, ls='--') #start
+
+        plt.axvline(x=end, ls='--') #start
+        plt.text(start, -1, f'Pose{pose}',fontsize=20)
+
+    plt.savefig(f'{path}/{point}.png')
+    plt.clf()
+    plt.close()
+    print('finished plot')
+    return 
+
+
+def main():
+
+    args = parse_args()
+    home_path = 'C:/Users/VIP/Desktop/yuru/yoga/'
+    data_path = os.path.join(home_path, args.data_path)
+    save_path = os.path.join(home_path, args.save_path)
+
+    if args.data_path is None or not os.path.exists(data_path):
+        print("Please provide a valid data path.")
+        print(data_path)
+        return
+    os.makedirs(save_path, exist_ok=True)
+
+    video_files = [f for f in os.listdir(data_path) if f.endswith('.MP4') ]#and f!='1_front.MP4'
+    # print(video_files)
+ 
+    df = pd.read_excel(home_path+'yoga-pose-classification/movement_frames_info.xlsx', sheet_name='train')
+
+    points = ['R_ankle', 'L_ankle', 'R_knee', 'L_knee', 'R_hip', 'L_hip', 'R_shoulder', 'L_shoulder', 'R_elbow', 'L_elbow', 'R_wrist', 'L_wrist']
+
+    for video in video_files:
+        print(video)
+        cap = cv2.VideoCapture(os.path.join(data_path, video))
+        if not cap.isOpened():
+            print(f"Error: Could not opne video {video}")
+            continue
+
+        video_name = os.path.splitext(video)[0]
+        rows = df.loc[df['VIDEO_name'] == video_name]
+        rows.reset_index(drop=True)
+
+        points_info = {'R_ankle':[], 'L_ankle': [], 'R_knee': [], 'L_knee': [], 'R_hip': [], 'L_hip': [],
+                        'R_shoulder': [], 'L_shoulder': [], 'R_elbow': [], 'L_elbow': [], 'R_wrist': [], 'L_wrist': []}
+        # print('here')
+        count=0
+        while cap.isOpened():
             
+            ret, frame = cap.read()
+            # fps = int(cap.get(cv2.CAP_PROP_FPS))
+            if not ret:
+                print("Ignoring empty camera frame.")
+                break
+            keypoint = detect_keypoints(frame)
+
             if keypoint :
                 # print(keypoint[28])
-                r_ankle.append( cal_angle(keypoint.landmark[28], keypoint.landmark[30], keypoint.landmark[32]) )
-                l_ankle.append( cal_angle(keypoint.landmark[27], keypoint.landmark[29], keypoint.landmark[31]) )
-                r_knee.append( cal_angle(keypoint.landmark[24], keypoint.landmark[26], keypoint.landmark[28]) )
-                l_knee.append( cal_angle(keypoint.landmark[23], keypoint.landmark[25], keypoint.landmark[27]) )
-                r_hip.append( cal_angle(keypoint.landmark[12], keypoint.landmark[24], keypoint.landmark[26]) )
-                l_hip.append( cal_angle(keypoint.landmark[11], keypoint.landmark[23], keypoint.landmark[25]) )
-                r_shoulder.append( cal_angle(keypoint.landmark[14], keypoint.landmark[12], keypoint.landmark[24]) )
-                l_shoulder.append( cal_angle(keypoint.landmark[13], keypoint.landmark[11], keypoint.landmark[23]) )
-                r_elbow.append( cal_angle(keypoint.landmark[16], keypoint.landmark[14], keypoint.landmark[12]) )
-                l_elbow.append( cal_angle(keypoint.landmark[15], keypoint.landmark[13], keypoint.landmark[11]) )
-                r_wrist.append( cal_angle(keypoint.landmark[20], keypoint.landmark[16], keypoint.landmark[14]) )
-                l_wrist.append( cal_angle(keypoint.landmark[19], keypoint.landmark[15], keypoint.landmark[13]) )
+                count+=1
+                # points_info['R_ankle'].append( cal_angle(keypoint.landmark[28], keypoint.landmark[30], keypoint.landmark[32]) )
+                # points_info['L_ankle'].append( cal_angle(keypoint.landmark[27], keypoint.landmark[29], keypoint.landmark[31]) )
+                # points_info['R_knee'].append( cal_angle(keypoint.landmark[24], keypoint.landmark[26], keypoint.landmark[28]) )
+                # points_info['L_knee'].append( cal_angle(keypoint.landmark[23], keypoint.landmark[25], keypoint.landmark[27]) )
+                # points_info['R_hip'].append( cal_angle(keypoint.landmark[12], keypoint.landmark[24], keypoint.landmark[26]) )
+                # points_info['L_hip'].append( cal_angle(keypoint.landmark[11], keypoint.landmark[23], keypoint.landmark[25]) )
+                # points_info['R_shoulder'].append( cal_angle(keypoint.landmark[14], keypoint.landmark[12], keypoint.landmark[24]) )
+                # points_info['L_shoulder'].append( cal_angle(keypoint.landmark[13], keypoint.landmark[11], keypoint.landmark[23]) )
+                # points_info['R_elbow'].append( cal_angle(keypoint.landmark[16], keypoint.landmark[14], keypoint.landmark[12]) )
+                # points_info['L_elbow'].append( cal_angle(keypoint.landmark[15], keypoint.landmark[13], keypoint.landmark[11]) )
+                # points_info['R_wrist'].append( cal_angle(keypoint.landmark[20], keypoint.landmark[16], keypoint.landmark[14]) )
+                # points_info['L_wrist'].append( cal_angle(keypoint.landmark[19], keypoint.landmark[15], keypoint.landmark[13]) )
+                points_info['R_ankle'].append( keypoint.landmark[28].x )
+                points_info['L_ankle'].append( keypoint.landmark[27].x )
+                points_info['R_knee'].append( keypoint.landmark[26].x )
+                points_info['L_knee'].append( keypoint.landmark[25].x )
+                points_info['R_hip'].append( keypoint.landmark[24].x )
+                points_info['L_hip'].append( keypoint.landmark[23].x )
+                points_info['R_shoulder'].append( keypoint.landmark[12].x )
+                points_info['L_shoulder'].append( keypoint.landmark[11].x )
+                points_info['R_elbow'].append( keypoint.landmark[14].x )
+                points_info['L_elbow'].append( keypoint.landmark[13].x )
+                points_info['R_wrist'].append( keypoint.landmark[16].x )
+                points_info['L_wrist'].append( keypoint.landmark[15].x)
             else:
-                r_ankle.append(-1)
-                l_ankle.append(-1)
-                r_knee.append(-1)
-                l_knee.append(-1)
-                r_hip.append(-1)
-                l_hip.append(-1)
-                r_shoulder.append(-1)
-                l_shoulder.append(-1)
-                r_elbow.append(-1)
-                l_elbow.append(-1)
-                r_wrist.append(-1)
-                l_wrist.append(-1)
+                points_info['R_ankle'].append(-1)
+                points_info['L_ankle'].append(-1)
+                points_info['R_knee'].append(-1)
+                points_info['L_knee'].append(-1)
+                points_info['R_hip'].append(-1)
+                points_info['L_hip'].append(-1)
+                points_info['R_shoulder'].append(-1)
+                points_info['L_shoulder'].append(-1)
+                points_info['R_elbow'].append(-1)
+                points_info['L_elbow'].append(-1)
+                points_info['R_wrist'].append(-1)
+                points_info['L_wrist'].append(-1)
+
+        cap.release()
+        print(f'frames={count}')
+        for point in points:
+            # print(points_info[point][:10])
+            plot_points(points_info[point], point, os.path.join(save_path, video_name), rows)
+
+if __name__ =='__main__':
+    main()
+
+
+
+
+
+# labels = sorted(os.listdir(data_path))
+# for label in labels:
+    # label_path = os.path.join(data_path, label)
+    # videos = os.listdir(label_path)
+    # for video in videos:
+    #     video_path = os.path.join(label_path, video)
+    #     frames = os.listdir(video_path)
+    #     joints = {r_ankle:[], l_ankle:[], r_knee:[], l_knee:[], r_hip:[], l_hip:[],
+    #               r_shoulder:[], l_shoulder:[], r_elbow:[], l_elbow:[], r_wrist:[], l_wrist:[]}
+    #     r_ankle = []
+    #     l_ankle = []
+    #     r_knee = []
+    #     l_knee = []
+    #     r_hip = []
+    #     l_hip = []
+    #     r_shoulder = []
+    #     l_shoulder = []
+    #     r_elbow = []
+    #     l_elbow = []
+    #     r_wrist = []
+    #     l_wrist = []
+        
+    #     for frame in frames:
+    #         frame_path = os.path.join(video_path, frame)
+    #         keypoint = detect_keypoints(frame_path)
+            
+    #         if keypoint :
+    #             # print(keypoint[28])
+    #             r_ankle.append( cal_angle(keypoint.landmark[28], keypoint.landmark[30], keypoint.landmark[32]) )
+    #             l_ankle.append( cal_angle(keypoint.landmark[27], keypoint.landmark[29], keypoint.landmark[31]) )
+    #             r_knee.append( cal_angle(keypoint.landmark[24], keypoint.landmark[26], keypoint.landmark[28]) )
+    #             l_knee.append( cal_angle(keypoint.landmark[23], keypoint.landmark[25], keypoint.landmark[27]) )
+    #             r_hip.append( cal_angle(keypoint.landmark[12], keypoint.landmark[24], keypoint.landmark[26]) )
+    #             l_hip.append( cal_angle(keypoint.landmark[11], keypoint.landmark[23], keypoint.landmark[25]) )
+    #             r_shoulder.append( cal_angle(keypoint.landmark[14], keypoint.landmark[12], keypoint.landmark[24]) )
+    #             l_shoulder.append( cal_angle(keypoint.landmark[13], keypoint.landmark[11], keypoint.landmark[23]) )
+    #             r_elbow.append( cal_angle(keypoint.landmark[16], keypoint.landmark[14], keypoint.landmark[12]) )
+    #             l_elbow.append( cal_angle(keypoint.landmark[15], keypoint.landmark[13], keypoint.landmark[11]) )
+    #             r_wrist.append( cal_angle(keypoint.landmark[20], keypoint.landmark[16], keypoint.landmark[14]) )
+    #             l_wrist.append( cal_angle(keypoint.landmark[19], keypoint.landmark[15], keypoint.landmark[13]) )
+    #         else:
+    #             r_ankle.append(-1)
+    #             l_ankle.append(-1)
+    #             r_knee.append(-1)
+    #             l_knee.append(-1)
+    #             r_hip.append(-1)
+    #             l_hip.append(-1)
+    #             r_shoulder.append(-1)
+    #             l_shoulder.append(-1)
+    #             r_elbow.append(-1)
+    #             l_elbow.append(-1)
+    #             r_wrist.append(-1)
+    #             l_wrist.append(-1)
 
         
-        print(f'finished {video} in {label}')
+    #     print(f'finished {video} in {label}')
